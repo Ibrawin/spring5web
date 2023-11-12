c938d9031a89d03f014d48ace3a06d3a2967067a
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-12 23:12:04 +0000

    add repository annotations to the repository classes for readability

--- src/main/java/com/ibrawin/spring5web/repositories/AuthorRepository.java
+++ src/main/java/com/ibrawin/spring5web/repositories/AuthorRepository.java
@@ -2,6 +2,8 @@ package com.ibrawin.spring5web.repositories;
 
 import com.ibrawin.spring5web.domain.Author;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- src/main/java/com/ibrawin/spring5web/repositories/BookRepository.java
+++ src/main/java/com/ibrawin/spring5web/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.ibrawin.spring5web.repositories;
 
 import com.ibrawin.spring5web.domain.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
