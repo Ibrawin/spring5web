c5d531c34f88f16edb0fd8e92ed42422a9729b09
Ibrawin <ibrahim-ayanbunmi@live.co.uk>
2023-11-13 22:37:37 +0000

    add author controller and web layer

---
+++ src/main/java/com/ibrawin/spring5web/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package com.ibrawin.spring5web.controllers;
+
+import com.ibrawin.spring5web.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
+}
